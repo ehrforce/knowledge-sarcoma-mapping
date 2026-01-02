16a64f99d7a982554935494445ad7f3a76755267
Bjørn Næss <bna@dips.no>
2026-01-02 02:25:24 +0100

    Downgrade to Prisma 6 for stability

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-    return new PrismaClient({
-        datasources: {
-            db: {
-                url: process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL
-            }
-        }
-    } as any)
+    return new PrismaClient()
 }
 
 declare global {
